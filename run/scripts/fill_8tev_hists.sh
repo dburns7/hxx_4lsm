831b8ffed8f56fbeafc1388dca25a20d6ba4d495
Dustin Burns <dburns@ucdavis.edu>
2014-11-03 07:28:15 -0800

    cleaned up analyzer

--- run/scripts/fill_8tev_hists.sh
+++ run/scripts/fill_8tev_hists.sh
@@ -2,12 +2,4 @@
 
 source COMPILE
 
-#./bin/AnalyzeHxx --8tev --fake_rate=1E-5 --num_smear=100 --met_smear=20 ../data/Analysis/CMSSW/DAS/8TeV/all.root ../results/8TeV/latest.root ../results/8TeV/
-
-#./bin/AnalyzeHxx --8tev --fake_rate=1E-5 --num_smear=100 --met_smear=20 ../data/Analysis/CMSSW/DAS_100k/8TeV/all.root ../results/8TeV/latest_100k.root ../results/8TeV/
-
-#./bin/AnalyzeHxx --8tev --fake_rate=1E-5 --num_smear=100 --met_smear=20 ../data/Analysis/CMSSW/DAS_Full/8TeV/all.root ../results/8TeV/latest_Full.root ../results/8TeV/
-
-#./bin/AnalyzeHxx --8tev --fake_rate=1E-5 --num_smear=100 --met_smear=20 ../data/Analysis/CMSSW/DAS_Full/8TeV/all.root ../results/8TeV/latest_Full_Combo.root ../results/8TeV/
-
-./bin/AnalyzeHxx --8tev --fake_rate=1E-5 --num_smear=100 --met_smear=20 ../data/Analysis/CMSSW/MCValidation/all.root ../results/8TeV/latest_MCValidation_4emuta.root ../results/8TeV/
+./bin/AnalyzeHxx ../data/all_data.root ../results/latest.root ../results
--- test/AnalyzeHxx.cpp
+++ test/AnalyzeHxx.cpp
@@ -14,7 +14,6 @@
 #include "TLorentzVector.h"
 
 #include "koptions.h"
-#include "hxx_tree.h"
 #include "histogram_manager.h"
 #include "cutflow_tool.h"
 
@@ -24,11 +23,6 @@ using namespace kutil;
 void usage(){
    cout << "usage:  AnalyzeHxx  [OPTIONS] <input_file> <output_root> <output_dir>\n";
    cout << "\n";
-   cout << "  --met_smear=<x>   : extra amount to smear MET.\n";
-   cout << "  --num_smear=<n>   : number of times to smear MET.\n";
-   cout << "  --fake_rate=<x>   : apply simple (whole event) fake rate <x> to W+jets.\n";
-   cout << "  --8tev            : 8 tev settings (default 14 tev).\n";
-   cout << "  --pub_plots       : generate publication plots.\n";
    exit(0);
 }
 
@@ -61,38 +55,12 @@ int main(int argc, char *argv[])
 {
    TRandom rng;
    rng.SetSeed(2014);
-   hxx_tree data;
    
    std::string opt, infile, outroot, outdir;
    koptions options(argc, argv);
    
    //check for the --help option:
    if ( options.find("--help") ) { usage(); }
-   //options.set("--seed=", seed);   
-   //if (seed > 0) rng.SetSeed(seed);
-   double met_smear = 30.0;
-   options.set("--met_smear=", met_smear);   
-   int    num_smear = 1;
-   options.set("--num_smear=", num_smear);   
-   double fake_rate = 0.0;
-   options.set("--fake_rate=", fake_rate);   
-   int mode_8tev = options.find("--8tev");
-   int pub_plots = options.find("--pub_plots");
-
-
-
-   double lumi = 300;
-   double met_cut = 150.0;
-
-   if (mode_8tev){
-     lumi = 20.0;
-     met_cut = 75;
-     cout << "INFO:  settings for sqrt(s) = 8 TeV\n";
-   } else {
-     cout << "INFO:  settings for sqrt(s) = 14 TeV\n";
-   }
-   cout << "INFO:  lumi:     " << lumi << "\n";
-   cout << "INFO:  MET cut:  " << met_cut << "\n";
 
    //check for unrecognized options (beginning with -- or -)
    while(options.unrecognized_option(opt)){
@@ -111,9 +79,6 @@ int main(int argc, char *argv[])
    cout << "INFO: reading analysis tree file:    " << infile  << "\n";
    cout << "INFO: writing histogram root file:   " << outroot << "\n";
    cout << "INFO: writing results to directory:  " << outdir  << "\n";
-   cout << "INFO: MET smearing amount is " << met_smear << "\n";
-   cout << "INFO: MET smearing number is " << num_smear << "\n";
-   cout << "INFO: W+jets fake rate is " << fake_rate << "\n";
 
    auto_write aw;
 
@@ -145,21 +110,19 @@ int main(int argc, char *argv[])
 
    TH1F hfit_bkg("sample_1","",  100,0.0,300.0);
    TH1F hfit_sig1("signal1","",  100,0.0,300.0);
-/*   TH1F hfit_sig21("signal21","",  100,0.0,300.0);
-   TH1F hfit_sig22("signal22","",  100,0.0,300.0);
-   TH1F hfit_sig23("signal23","",  100,0.0,300.0);
-   TH1F hfit_sig24("signal24","",  100,0.0,300.0);
-*/
+   TH1F hfit_sig10("signal10","",  100,0.0,300.0);
+   TH1F hfit_sig100("signal100","",  100,0.0,300.0);
+   TH1F hfit_sig500("signal500","",  100,0.0,300.0);
+   TH1F hfit_sig1000("signal1000","",  100,0.0,300.0);
+
    int    nsig = 0;
    double wsig = 0.0;
    hfit_bkg.Sumw2();
    hfit_sig1.Sumw2();
-/*   hfit_sig21.Sumw2();
-   hfit_sig22.Sumw2();
-   hfit_sig23.Sumw2();
-   hfit_sig24.Sumw2();
-*/
-/*
+   hfit_sig10.Sumw2();
+   hfit_sig100.Sumw2();
+   hfit_sig500.Sumw2();
+   hfit_sig1000.Sumw2();
    
    //control plots (no pre-selection)
    histogram_manager hcnjet  (new TH1F("hcnjet",  "", 10,  0.0, 10.0),     h0mll, aw);
@@ -169,65 +132,20 @@ int main(int argc, char *argv[])
    //test new variables
    histogram_manager htestx  (new TH1F("htestx",  "", 25,  0.0, 3.2),      h0mll, aw);
    histogram_manager htesty  (new TH1F("htesty",  "", 25,  0.0, 200.0),    h0mll, aw);
-*/
-
-   //histograms at stage 0
-   histogram_manager h0ID       (new TH1F("h0ID",     "", 25,  0.0, 25.0), h0mll, aw);
-   
-   histogram_manager h0ept      (new TH1F("h0ept",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0eeta     (new TH1F("h0eeta",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0ephi     (new TH1F("h0ephi",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0ech      (new TH1F("h0ech",    "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0ecnt     (new TH1F("h0ecnt",   "", 10,   0.0, 10.0),  h0mll, aw);
-   
-   histogram_manager h0mpt      (new TH1F("h0mpt",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0meta     (new TH1F("h0meta",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0mphi     (new TH1F("h0mphi",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0mch      (new TH1F("h0mch",    "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0mcnt     (new TH1F("h0mcnt",   "", 10,   0.0, 10.0),  h0mll, aw);
-   
-   histogram_manager h0l1pt      (new TH1F("h0l1pt",    "", 200,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0l2pt      (new TH1F("h0l2pt",    "", 200,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0l3pt      (new TH1F("h0l3pt",    "", 200,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0l4pt      (new TH1F("h0l4pt",    "", 200,  0.0, 250.0), h0mll, aw);
-   
-   histogram_manager h0gpt      (new TH1F("h0gpt",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0geta     (new TH1F("h0geta",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0gphi     (new TH1F("h0gphi",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0gch      (new TH1F("h0gch",    "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0gcnt     (new TH1F("h0gcnt",   "", 10,   0.0, 10.0),  h0mll, aw);
-   
-   histogram_manager h0mee      (new TH1F("h0mee",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0mmm      (new TH1F("h0mmm",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0mgg      (new TH1F("h0mgg",    "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0mll_l    (new TH1F("h0mll_l",  "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0mll_sl   (new TH1F("h0mll_sl", "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0mllll    (new TH1F("h0mllll",  "", 100,  0.0, 250.0), h0mll, aw);
-   
-   histogram_manager h0met_nopu (new TH1F("h0met_nopu", "", 100,  0.0, 250.0), h0mll, aw);
-   histogram_manager h0met_pu   (histprop(new TH1F("h0met_pu",   "", 100,  0.0, 250.0)), h0mll, aw);
-   histogram_manager h0met_nopu_phi (new TH1F("h0met_nopu_phi", "", 60,   -5.0, 5.0),  h0mll, aw);
-   histogram_manager h0met_pu_phi   (new TH1F("h0met_pu_phi",   "", 60,   -5.0, 5.0),  h0mll, aw);
-   
-   //histograms after analysis cuts
-   histogram_manager h1met_nopu (new TH1F("h1met_nopu", "", 100,  0.0, 250.0), h0mll, aw);
  
    cout << "INFO: opening file: " << infile << "\n";
 
    TFile * file = new TFile(infile.c_str());
-   TDirectoryFile * dir = (TDirectoryFile*) file->Get("demo");
-   //TDirectoryFile	demo;1	demo
-   TTree * tree = (TTree*) dir->Get("hxxtree");
+   TTree * data = (TTree*) file->Get("SelectedTree");
 
-   if (! tree) {
+   if (! data) {
       cout << "ERROR:  could not open tree.\n";
       exit(0);
    }
+   
+   long int numberOfEntries = data->GetEntries();
 
-   data.ReadTree(tree);
-   long int numberOfEntries = tree->GetEntries();
-
-   //tree->Print();
+   data->Print();
   
    int count = 0;
    int nupdate = numberOfEntries / 20;
@@ -249,148 +167,28 @@ int main(int argc, char *argv[])
          iprescale = 0;
       }
       
-      tree->GetEntry(entry);
+      data->GetEntry(entry);
 
-      h0ID.Fill(data.sample, data.sample);
-
-      cutflow.increment(0, data.sample, data.weight);      
-      h0mll.Fill(data.sample, data.mll, data.weight);
-      
-     //Fill single lepton hists at stage 0 
-     for(int i=0; i<data.nelec; i++){
-        //cout << data.nelec << " " << data.elec_pt->size() << " " << data.sample << endl;
-        h0ept  .Fill(data.sample, data.elec_pt->at(i),     data.weight);
-        h0eeta .Fill(data.sample, data.elec_eta->at(i),    data.weight);
-        h0ephi .Fill(data.sample, data.elec_phi->at(i),    data.weight);
-        h0ech  .Fill(data.sample, data.elec_ch->at(i),     data.weight);
-      }
-      h0ecnt   .Fill(data.sample, data.nelec,    data.weight);
-      for(int i=0; i<data.nmuon; i++){
-        h0mpt  .Fill(data.sample, data.muon_pt->at(i),     data.weight);
-        h0meta .Fill(data.sample, data.muon_eta->at(i),    data.weight);
-        h0mphi .Fill(data.sample, data.muon_phi->at(i),    data.weight);
-        h0mch  .Fill(data.sample, data.muon_ch->at(i),     data.weight);
-      }
-      h0mcnt   .Fill(data.sample, data.nmuon,    data.weight);
-
-     //Fill single photon hists at stage 0 
-     for(int i=0; i<data.nphot; i++){
-        h0gpt  .Fill(data.sample, data.phot_pt->at(i),     data.weight);
-        h0geta .Fill(data.sample, data.phot_eta->at(i),    data.weight);
-        h0gphi .Fill(data.sample, data.phot_phi->at(i),    data.weight);
-      }
-      h0gcnt   .Fill(data.sample, data.nphot,    data.weight);
-      
-      //Fill multiparticle hists at stage 0
-      double mll_l, mll_sl, mllll;
-      if(data.nelec == 4 && data.nmuon == 0){
-        double mee1, mee2, mee_l, mee_sl, meeee, ept1, ept2, ept3, ept4;
-        kinematic_vars_4e(data, mee1, mee2, mee_l, mee_sl, meeee, ept1, ept2, ept3, ept4);
-        h0l1pt   .Fill(data.sample, ept1,   data.weight);
-        h0l2pt   .Fill(data.sample, ept2,   data.weight);
-        h0l3pt   .Fill(data.sample, ept3,   data.weight);
-        h0l4pt   .Fill(data.sample, ept4,   data.weight);
-        h0mee    .Fill(data.sample, mee1,   data.weight);
-        h0mee    .Fill(data.sample, mee2,   data.weight);
-        h0mll_l  .Fill(data.sample, mee_l,  data.weight);
-        h0mll_sl .Fill(data.sample, mee_sl, data.weight);
-        h0mllll  .Fill(data.sample, meeee,  data.weight);
-        mll_l  = mee_l;
-        mll_sl = mee_sl;
-        mllll  = meeee;
-      } 
-      if(data.nelec == 0 && data.nmuon == 4){
-        double mmm1, mmm2, mmm_l, mmm_sl, mmmmm;
-        kinematic_vars_4m(data, mmm1, mmm2, mmm_l, mmm_sl, mmmmm);
-        h0mmm    .Fill(data.sample, mmm1,   data.weight);
-        h0mmm    .Fill(data.sample, mmm2,   data.weight);
-        h0mll_l  .Fill(data.sample, mmm_l,  data.weight);
-        h0mll_sl .Fill(data.sample, mmm_sl, data.weight);
-        h0mllll  .Fill(data.sample, mmmmm,  data.weight);
-        mll_l  = mmm_l;
-        mll_sl = mmm_sl;
-        mllll  = mmmmm;
-      }
-      if(data.nelec == 2 && data.nmuon == 2){
-        double mee, mmm, m_l, m_sl, meemm;
-        kinematic_vars_2e2m(data, mee, mmm, m_l, m_sl, meemm);
-        h0mee    .Fill(data.sample, mee,    data.weight);
-        h0mmm    .Fill(data.sample, mmm,    data.weight);
-        h0mll_l  .Fill(data.sample, m_l,    data.weight);
-        h0mll_sl .Fill(data.sample, m_sl,   data.weight);
-        h0mllll  .Fill(data.sample, meemm,  data.weight);
-        mll_l  = m_l;
-        mll_sl = m_sl;
-        mllll  = meemm;
-      }
-      double mgg;
-      if(data.nphot == 2){
-        kinematic_vars_2g(data, mgg);
-	h0mgg    .Fill(data.sample, mgg,    data.weight);
-      }
-
-      //Fill other hists at stage 0
-      h0met_nopu     .Fill(data.sample, data.nopu_met, data.weight);
-      h0met_nopu_phi .Fill(data.sample, data.nopu_met_phi, data.weight);
-
-
-      cutflow.increment(1, data.sample, data.weight);
-      
-      // Leading dilepton mass cut:
-      if(mll_l < 50.0 || mll_l > 106.0) continue;      
-      cutflow.increment(2, data.sample, data.weight);
-      
-      // Subleading dilepton mass cut:
-      if(mll_sl < 12.0 || mll_sl > 115.0) continue;
-      cutflow.increment(3, data.sample, data.weight);
-    
-      // Four lepton mass cut:
-      if(mllll < 105.0 || mllll > 145.0) continue;
-      cutflow.increment(4, data.sample, data.weight);
-
-      // Met cut:
-      if(data.nopu_met < 75.0) continue;
-      cutflow.increment(5, data.sample, data.weight);
-
-    
-      //Fill histograms after analysis cuts
-      h1met_nopu. Fill(data.sample, data.nopu_met, data.weight);
+      cutflow.increment(0, 1, 1);      
+      //cutflow.increment(0, data.sample, data.weight);      
 
       // fill limit setting histograms:
-      if (data.sample < 20)  hfit_bkg.Fill(data.nopu_met, data.weight);
-      if (data.sample == 20) hfit_sig1.Fill(data.nopu_met, data.weight);
-
+      //if (data.sample < 20)  hfit_bkg.Fill(data.nopu_met, data.weight);
+      //if (data.sample == 20) hfit_sig1.Fill(data.nopu_met, data.weight);
    }
    cout << "\n";
 
-   cout << "SUMMARY:  read " << count << " of " << tree->GetEntries() << " events from analysis tree.\n";
+   cout << "SUMMARY:  read " << count << " of " << data->GetEntries() << " events from analysis tree.\n";
    
    TFile * foutroot = new TFile(outroot.c_str(), "RECREATE");
    foutroot->cd();
    aw.WriteAll();
    foutroot->Close();
 
-   //cout << "SUMMARY:  done writing files.\n";
+   cout << "SUMMARY:  done writing files.\n";
 
-
-////////////////////// 4l //////////////////////////
-   cout << "Cutflow:  Stage 0 (llll preselection)\n";
+   cout << "Cutflow:  Stage 0 \n";
    cutflow.print(0);
-
-   cout << "Cutflow:  Stage 1 (after single lepton cuts)\n";
-   cutflow.print(1);
-
-   cout << "Cutflow:  Stage 2 (after mll_l cut) \n";
-   cutflow.print(2);
-
-   cout << "Cutflow:  Stage 3 (after mll_sl cut) \n";
-   cutflow.print(3);
-    
-   cout << "Cutflow:  Stage 4 (after mllll cut) \n";
-   cutflow.print(4);
-
-   cout << "Cutflow:  Stage 5 (after met cut) \n";
-   cutflow.print(5);
 
    char name[100];
    TFile * f = NULL;
@@ -404,10 +202,10 @@ int main(int argc, char *argv[])
    hfit_bkg.Write();
    f->Close();
 
-   /*sprintf(name, "%s/mchi10.root", outdir.c_str());
+   sprintf(name, "%s/mchi10.root", outdir.c_str());
    f = new TFile(name, "RECREATE");
    f->cd();
-   h = (TH1F *) hfit_sig21.Clone("signal");
+   h = (TH1F *) hfit_sig10.Clone("signal");
    h->Write();
    hfit_bkg.Write();
    f->Close();
@@ -415,7 +213,7 @@ int main(int argc, char *argv[])
    sprintf(name, "%s/mchi100.root", outdir.c_str());
    f = new TFile(name, "RECREATE");
    f->cd();
-   h = (TH1F *) hfit_sig22.Clone("signal");
+   h = (TH1F *) hfit_sig100.Clone("signal");
    h->Write();
    hfit_bkg.Write();
    f->Close();
@@ -423,7 +221,7 @@ int main(int argc, char *argv[])
    sprintf(name, "%s/mchi500.root", outdir.c_str());
    f = new TFile(name, "RECREATE");
    f->cd();
-   h = (TH1F *) hfit_sig23.Clone("signal");
+   h = (TH1F *) hfit_sig500.Clone("signal");
    h->Write();
    hfit_bkg.Write();
    f->Close();
@@ -431,12 +229,10 @@ int main(int argc, char *argv[])
    sprintf(name, "%s/mchi1000.root", outdir.c_str());
    f = new TFile(name, "RECREATE");
    f->cd();
-   h = (TH1F *) hfit_sig24.Clone("signal");
+   h = (TH1F *) hfit_sig1000.Clone("signal");
    h->Write();
    hfit_bkg.Write();
    f->Close();
-*/
-
 
 }
 
